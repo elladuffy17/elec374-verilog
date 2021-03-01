20e82c1ebab511c78a41bce9034b3353236fa7e8
elladuffy17 <72311928+elladuffy17@users.noreply.github.com>
2021-03-01 15:38:26 -0500

    2-to-1 multiplexer

---
+++ mux_2X1.v
@@ -0,0 +1,15 @@
+module mux_2X1(
+	input [31:0] in0, in1, 
+	input selectSignal, 
+	output reg [31:0] out);
+
+  //multiplexer is used to switch one of the several input lines to a single common output line
+	always@(*) //continuous loop
+	begin
+		if (selectSignal == 1)
+			out[31:0] <= in1[31:0];
+		else
+			out[31:0] <= in0[31:0];
+		end
+	endmodule
+ 
