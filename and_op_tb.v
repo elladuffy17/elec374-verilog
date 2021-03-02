563f2013f59b3d11195bf7c2b6d47ed2f8a4779d
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-02 14:59:09 -0500

    Update and_op_tb.v

--- and_op_tb.v
+++ and_op_tb.v
@@ -10,7 +10,7 @@ module and_op_tb;
 	reg Clock;
 	reg [31:0] Mdatain;
 
-// add any other signals to see in your simulation
+	// add any other signals to see in your simulation
 
 	parameter Default = 4’b0000, Reg_load1a = 4’b0001, Reg_load1b = 4’b0010, Reg_load2a = 4’b0011, Reg_load2b = 4’b0100, Reg_load3a = 4’b0101, Reg_load3b = 4’b0110, T0 = 4’b0111, T1 = 4’b1000, T2 = 4’b1001, T3 = 4’b1010, T4 = 4’b1011, T5 = 4’b1100;
 	reg [3:0] Present_state = Default;
