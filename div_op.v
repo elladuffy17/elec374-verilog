a1dbb94a82bb53b52435b487726fe90d01563b5d
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-01 20:49:40 -0500

    Update div_op.v

--- div_op.v
+++ div_op.v
@@ -1,15 +1,15 @@
 //div_op.v, 32-bit DIVISION operation module
 
-module div_op (input signed [31:0] divisor, dividend, output reg [32*2-1:0] quotient);
+module div_op (input signed [31:0] divisor, dividend, output reg [32*2-1:0] result);
 
-	reg [63:32] top, bottom; //splits the output reg quotient into two sections from 0:31 bits and 32:64 bits
+	reg [63:32] hi, lo; //splits the output reg quotient into two sections from 0:31 bits and 32:64 bits
 	always @ (*) //'*' indicates all variables are considered in sensitivity list, behaves like combinational logic 
 	begin
-		top = dividend % divisor;
-		bottom = (dividend - top) / divisor;
+		hi = dividend % divisor;
+		lo = (dividend - hi) / divisor;
 		
 		begin
-			quotient = {top, bottom};
+			result = {hi, lo};
 		end
 
 	end
