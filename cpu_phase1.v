d7fde8074eeea7d636e0025d6a5bc163408e0d27
elladuffy17 <72311928+elladuffy17@users.noreply.github.com>
2021-03-03 12:39:27 -0500

    Update cpu_phase1.v

--- cpu_phase1.v
+++ cpu_phase1.v
@@ -78,13 +78,63 @@ module cpuPhase1(
   wire [31:0] busMuxInInPort;
   wire [31:0] busMuxInC;
   
-  /* design/instantiate the registers RO to R15, PC, IR, Y, Z, MAR, HI and LO.
-  first, we want to enable signals for registers*/
+  
+  //enable signals for registers
   wire R1Enable, R2Enable, R3Enable, R4Enable, R5Enable, R6Enable, R7Enable, R8Enable, R9Enable, 
-  R10Enable, R11Enable, R12Enable, R13Enable, R14Enable, R15Enable, hiEnable, loEnable, zHighEnable, 
-  zLowEnable, pcEnable, MDREnable, inPortEnable, CEnable; 
+  R10Enable, R11Enable, R12Enable, R13Enable, R14Enable, R15Enable, hiEnable, loEnable, zHiEnable, 
+  zLoEnable, pcEnable, MDREnable, inPortEnable, CEnable; 
   
   wire [31:0] busMuxOut; //feed into registers as the input from the bus
   
   GPReg R1(clk, clr, R1In, busMuxOut)
+  
+  /* design/instantiate the registers*/
+  GPReg R0(busMuxInR0, clk, clr, 1'd0, busMuxOut);
+  GPReg R1(busMuxInR1, clk, clr, R1Enable, busMuxOut);
+  GPReg R2(busMuxInR2, clk, clr, R2Enable, busMuxOut);
+  GPReg R3(busMuxInR3, clk, clr, R3Enable, busMuxOut);
+  GPReg R4(busMuxInR4, clk, clr, R4Enable, busMuxOut);
+  GPReg R5(busMuxInR5, clk, clr, R5Enable, busMuxOut);
+  GPReg R6(busMuxInR6, clk, clr, R6Enable, busMuxOut);
+  GPReg R7(busMuxInR7, clk, clr, R7Enable, busMuxOut);
+  GPReg R8(busMuxInR8, clk, clr, R8Enable, busMuxOut);
+  GPReg R9(busMuxInR9, clk, clr, R9Enable, busMuxOut);
+  GPReg R10(busMuxInR10, clk, clr, R10Enable, busMuxOut);
+  GPReg R11(busMuxInR11, clk, clr, R11Enable, busMuxOut);
+  GPReg R12(busMuxInR12, clk, clr, R12Enable, busMuxOut);
+  GPReg R13(busMuxInR13, clk, clr, R13Enable, busMuxOut);
+  GPReg R14(busMuxInR14, clk, clr, R14Enable, busMuxOut);
+  GPReg R15(busMuxInR15, clk, clr, R15Enable, busMuxOut);
+  
+  GPReg hiReg(busMuxInHi, clk, clr, hiEnable, busMuxOut);
+  GPReg loReg(busMuxInLo, clk, clr, loEnable, busMuxOut);
+  GPReg zHiReg(busMuxInZHi, clk, clr, zHiEnable, busMuxOut);
+  GPReg zLoReg(busMuxInZLo, clk, clr, zLoEnable, busMuxOut);
+  GPReg pcReg(busMuxInPC, clk, clr, pcEnable, busMuxOut);
+  GPReg inPortReg(busMuxInInPort, clk, clr, inPortEnable, busMuxOut);
+  GPReg cReg(busMuxInC, clk, clr, CEnable, busMuxOut);
+  
+  mux_2X1 MDRmux (busMuxOutMDR, mDataIn, MDRread, muxOut);
+  GPReg MDRReg(busMuxInMDR, clk, clr, MDREnable, busMuxOutMDR);
+  
+  //now, instantiate the bus
+  
+  bus phase1bus(busMuxOut, busMuxInR0, busMuxInR1, busMuxInR2, busMuxInR3, busMuxInR4,
+                busMuxInR5, busMuxInR6, busMuxInR7, busMuxInR8, busMuxInR9, busMuxInR10,
+                busMuxInR11, busMuxInR12, busMuxInR13, busMuxInR14, busMuxInR15, busMuxInHi,
+                busMuxInLo, busMuxInZHi, busMuxInZLo, busMuxInPC, busMuxInMDR, busMuxInInPort,
+                busMuxInC, R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out, R8Out, R9Out,
+                R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, hiOut, loOut, zHighOut, zLoOut, 
+                pcOut, MDRout, inPortOut, Cout = 0;
+  input R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out,
+  R8Out, R9Out, R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, hiOut, loOut,
+                zHighOut, zLoOut, pcOut, MDRout, inPortOut, Cout);
+  
+  //MAR, IR, ???
+  //alu
+  
+  end module
+    
+  
+  
   
