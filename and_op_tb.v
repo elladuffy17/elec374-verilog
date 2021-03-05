c82ac0368116a4b18263ff149d04543083354250
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-05 15:13:54 -0500

    Update and_op_tb.v

--- and_op_tb.v
+++ and_op_tb.v
@@ -6,13 +6,13 @@ module and_op_tb;
 
 	reg PCout, Zlowout, MDRout, R2out, R4out; 
 	reg MARin, Zin, PCin, MDRin, IRin, Yin;
-	reg IncPC, Read, AND R5in, R2in, R4in; //'AND' changes based on operation we want to test
+	reg IncPC, Read, AND, R5in, R2in, R4in; //'AND' changes based on operation we want to test
 	reg Clock;
 	reg [31:0] Mdatain;
 
 	// add any other signals to see in your simulation
 
-	parameter Default = 4’b0000, Reg_load1a = 4’b0001, Reg_load1b = 4’b0010, Reg_load2a = 4’b0011, Reg_load2b = 4’b0100, Reg_load3a = 4’b0101, Reg_load3b = 4’b0110, T0 = 4’b0111, T1 = 4’b1000, T2 = 4’b1001, T3 = 4’b1010, T4 = 4’b1011, T5 = 4’b1100;
+	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
 	reg [3:0] Present_state = Default;
 
 	phase_1 DUT(PCout, Zlowout, MDRout, R2out, R4out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, AND, R5in, R2in, R4in, Clock, Mdatain);
@@ -121,7 +121,7 @@ module and_op_tb;
 	
 				T4: begin
 					#10 R4out <= 1; AND <= 1; Zin <= 1;
-					#15 R4 out <= 0; AND <= 0; Zin <= 0;
+					#15 R4out <= 0; AND <= 0; Zin <= 0;
 				end
 
 				T5: begin
