87618970901ba95ccf54b446b2cc70c2c06f8bbb
elladuffy17 <72311928+elladuffy17@users.noreply.github.com>
2021-03-05 21:11:26 -0500

    Update shl_op.v

--- shl_op.v
+++ shl_op.v
@@ -6,6 +6,7 @@ module shl_op (
 	input wire [31:0] numOfShifts,
 	output wire [31:0] data_out
 );
-	assign data_out[31:0] = data_in << numOfShifts; //shifts the input data the given number of shifts to the left and the result is set to data_out, logical shift fills shift with zeros
+	//shifts the input data the given number of shifts to the left and the result is set to data_out, logical shift fills shift with zeros
+	assign data_out[31:0] = data_in << numOfShifts; 
 
 endmodule
