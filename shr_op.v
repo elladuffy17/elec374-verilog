fdc9b833b8f92ec2d6e71d1a8e50fa380dbf54ac
elladuffy17 <72311928+elladuffy17@users.noreply.github.com>
2021-03-05 21:10:57 -0500

    Update shr_op.v

--- shr_op.v
+++ shr_op.v
@@ -4,8 +4,9 @@
 module shr_op (
 	input wire [31:0] data_in,
 	input wire [31:0] numOfShifts,
-	output wire [31:0] datta_out
+	output wire [31:0] data_out
 );
-	assign data_out[31:0] = data_in >> numOfShifts; //shifts the input data the given number of shifts to the right and the result is set to data_out, logical shift fills shift with zeros
+//shifts the input data the given number of shifts to the right and the result is set to data_out, logical shift fills shift with zeros
+	assign data_out[31:0] = data_in >> numOfShifts; 
 
 endmodule
