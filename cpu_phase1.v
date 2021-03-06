d8b5e4fe52d0a92657bbf5d1151314d5b19e58f6
elladuffy17 <72311928+elladuffy17@users.noreply.github.com>
2021-03-05 21:04:58 -0500

    Update cpu_phase1.v

--- cpu_phase1.v
+++ cpu_phase1.v
@@ -7,9 +7,30 @@ the instructions */
 
 module cpuPhase1(
   //the input (in.port) and output (out.port) connects the CPU to the outside world
-  output wire [31:0] inPortOut,
-  input wire [31:0] inPortIn,
-  input wire clk, clr, mDataIn,
+ 
+   input PCout,
+	input ZHighout,
+	input ZLowout,
+	input MDRout,
+	input R2Out,
+	input R4Out,
+	input MARin,
+	input PCin,
+	input MDRin,
+	input IRin,
+	input Yin,
+	input IncPC,
+	input MDRRead,
+   input wire [4:0] operation,
+	input busMuxInR5,
+	input busMuxInR2,
+	input busMuxInR4,
+	input clk, 
+	input [31:0] mDataIn
+	//input wire [31:0] inPortIn,
+	//output wire [31:0] inPortOut
+
+ 
 );
   
   //define 32-to-5 encoder input/output wires
@@ -19,9 +40,9 @@ module cpuPhase1(
   //define and initialize those 32 input wires for the 32-to-5 encoder
   wire R0Out = 0;
   wire R1Out = 0;
-  wire R2Out = 0;
+  //wire R2Out = 0;
   wire R3Out = 0;
-  wire R4Out = 0;
+  //wire R4Out = 0;
   wire R5Out = 0;
   wire R6Out = 0;
   wire R7Out = 0;
@@ -35,17 +56,19 @@ module cpuPhase1(
   wire R15Out = 0;
   wire hiOut = 0;
   wire loOut = 0;
-  wire zHighOut = 0;
-  wire zLoOut = 0;
-  wire pcOut = 0;
-  wire MDRout = 0;
+  //wire zHighOut = 0;
+  //wire zLoOut = 0;
+  //wire pcOut = 0;
+  //wire MDRout = 0;
   wire inPortOut = 0;
   wire Cout = 0;
+  
+  reg clear;
   
   //connect the input wires to the encoder using the concatenation operator
   //assign statement is used since these statements are always active
   //note, {8{1'b0}} is 8 zero bits
-  assign encoderInput = {{8{1'b0}}, Cout, inPortOut, MDRout, pcOut, zLoOut, zHighOut, loOut, hiOut, R15Out,
+  assign encoderInput = {{8{1'b0}}, Cout, inPortOut, MDRout, PCout, ZLowout, ZHighout, loOut, hiOut, R15Out,
                          R14Out, R13Out, R12Out, R11Out, R10Out, R9Out, R8Out, R7Out, R6Out, R5Out, 
                          R4Out, R3Out, R2Out, R1Out, R0Out};
   
@@ -55,10 +78,10 @@ module cpuPhase1(
   //inputs to the bus MUX (from our registers, output q)
   wire [31:0] busMuxInR0;
   wire [31:0] busMuxInR1;
-  wire [31:0] busMuxInR2;
+  //wire [31:0] busMuxInR2;
   wire [31:0] busMuxInR3;
-  wire [31:0] busMuxInR4;
-  wire [31:0] busMuxInR5;
+  //wire [31:0] busMuxInR4;
+  //wire [31:0] busMuxInR5;
   wire [31:0] busMuxInR6;
   wire [31:0] busMuxInR7;
   wire [31:0] busMuxInR8;
@@ -86,7 +109,6 @@ module cpuPhase1(
   
   wire [31:0] busMuxOut; //feed into registers as the input from the bus
   
-  GPReg R1(clk, clr, R1In, busMuxOut)
   
   /* design/instantiate the registers*/
   GPReg R0(busMuxInR0, clk, clr, 1'd0, busMuxOut);
@@ -126,23 +148,16 @@ module cpuPhase1(
                 busMuxInLo, busMuxInZHi, busMuxInZLo, busMuxInPC, busMuxInMDR, busMuxInInPort,
                 busMuxInC, R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out, R8Out, R9Out,
                 R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, hiOut, loOut, zHighOut, zLoOut, 
-                pcOut, MDRout, inPortOut, Cout
-  input R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out,
-  R8Out, R9Out, R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, hiOut, loOut,
-                zHighOut, zLoOut, pcOut, MDRout, inPortOut, Cout);
+                pcOut, MDRout, inPortOut, Cout);
   
   //MAR, IR, ???
   //alu
-  alu aluPhase1(
-  .clock(clk),
-	.clear(clr), 
-	.regA(busMuxInY),
-  .regB(busMuxOut),
-  5'b1000,
-	.regZ(Cout)
-	);
   
-  end module
+  alu aluPhase1(.regZ(busMuxOut), .clock(clk), .clear(clr), .regA(busMuxInY), .regB(busMuxInY), .opcode(operation));
+  
+  endmodule
+    
+  
     
   
   
