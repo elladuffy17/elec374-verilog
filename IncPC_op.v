63de292a5157bf4f9be992b5fd42321c39673d5a
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-09 11:42:11 -0500

    Create IncPC_op.v

---
+++ IncPC_op.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module IncPC_op(
+  input clk, IncPC, enable,
+	input [31:0] inputPC,
+	output reg[31:0] newPC
+	);
+	
+always @ (posedge clk)
+	begin
+		if(IncPC == 1 && enable ==1)
+			newPC <= newPC + 1;
+		else if (enable == 1)
+			newPC <= inputPC;
+	end
+				
+endmodule
