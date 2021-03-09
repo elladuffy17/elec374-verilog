e359028d17dbf36fb9acb865d1bf7baa21aff53b
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-09 11:44:01 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -7,6 +7,7 @@ module alu (
 	
 	input clock,
 	input clear, 
+	input IncPC,
 
 	input wire [31:0] regA,
 	input wire [31:0] regB,
@@ -17,7 +18,7 @@ module alu (
 	parameter alu_add = 5'b00011, alu_sub = 5'b00100, alu_shr = 5'b00101, alu_shl = 5'b00110, alu_ror = 5'b00111, alu_rol = 5'b01000, alu_and = 5'b01001, alu_or = 5'b01010, alu_mult = 5'b01110, 
 	alu_div = 5'b01111, alu_neg = 5'b10000, alu_not = 5'b10001; //following the opcode provided in the CPU_spec file provided for the lab
 
-	wire [31:0] add_op_sum, add_op_cout, sub_op_sum, sub_op_cout, shr_op_out, shl_op_out, ror_op_out, rol_op_out, and_op_out, or_op_out, neg_op_out, not_op_out;
+	wire [31:0] IncPC_out, add_op_sum, add_op_cout, sub_op_sum, sub_op_cout, shr_op_out, shl_op_out, ror_op_out, rol_op_out, and_op_out, or_op_out, neg_op_out, not_op_out;
 	wire [63:0] mult_op_out, div_op_out;
 
 	always @ (*)
@@ -98,7 +99,8 @@ module alu (
 	div_op div_M0(regA, regB, div_op_out);
 	neg_op neg_M0(regB, neg_op_out); //shown in regB in simplified datapath given for lab
 	not_op not_M0(regB, not_op_out); //shown in regB in simplified datapath given for lab
-
+	IncPC_op IncPC_M0(regA, IncPC, IncPC_out);
+	
 endmodule
 	
 
