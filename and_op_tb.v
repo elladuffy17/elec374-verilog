1f5546fccf364f6b30a94b16ff41d442ef5f328a
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-09 15:52:03 -0500

    Update and_op_tb.v

--- and_op_tb.v
+++ and_op_tb.v
@@ -1,5 +1,7 @@
 //add_op_tb.v, testbench for the AND operation module 
 
+//add_op_tb.v, testbench for the AND operation module 
+
 `timescale 1ns/10ps
 
 module and_op_tb;
@@ -18,7 +20,7 @@ module and_op_tb;
 	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
 	reg [3:0] Present_state = Default;
 
-	cpuPhase1 DUT(PCout, ZHighout, Zlowout, MDRout, R2out, R4out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, AND, R5in, R2in, R4in, Clock, Mdatain);
+	cpu_phase1 DUT(PCout, ZHighout, Zlowout, MDRout, R2out, R4out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, AND, R5in, R2in, R4in, Clock, Mdatain);
 	 //the input (in.port) and output (out.port) connects the CPU to the outside world
    //the input (in.port) and output (out.port) connects the CPU to the outside world
 
@@ -32,7 +34,7 @@ module and_op_tb;
 	always @(posedge Clock) // finite state machine; if clock rising-edge
 		begin
 			case(Present_state)
-				
+
 				Default			:	#40 Present_state = Reg_load1a;
 
 				Reg_load1a		:	#40 Present_state = Reg_load1b;
@@ -58,9 +60,9 @@ module and_op_tb;
 				T4					:	#40 Present_state = T5;
 
 
-				
+
 			endcase
-			
+
 		end
 
 	always @ (Present_state) //do the required job in each state
@@ -113,7 +115,7 @@ module and_op_tb;
 					Mdatain <= 32'h00000007;
 					PCin <= 1; MDRout <= 1;
 
-					#10 PCout <= 1; MARin <= 1; lncPC <= 1; Zin <= 1; //she has ZLowIn <= 1; but its commented out and no Zin in this line
+					#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1; //she has ZLowIn <= 1; but its commented out and no Zin in this line
 					#15 PCin <= 0; MDRout <= 0; PCout <= 0; MARin <= 0; IncPC <= 0;
 				end
 
@@ -121,26 +123,18 @@ module and_op_tb;
 					Mdatain <= 32'h4A920000;   
 					Read <= 1; MDRin <= 1;
 					#10 Read <= 0; MDRin <= 0;
-					//Mdatain <= 32'h00000007;
-					//PCin <= 1; MDRout <= 1;
-				
-					//#10 PCout <= 1; MARin <= 1; IncPC <= 1;
-					//#15 PCin <= 0; MDRout <= 0; PCout <= 0; MARin <= 0; IncPC <= 0;
-
-					//Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-					//Mdatain <= 32'h4A920000; //opcode for "and R5, R2, R4"
 				end
 
 				T2: begin
-					MDRin <= 1; IRin <= 1;
-					#15 MDRin <= 0; IRin <= 0;
+					#10 MDRin <= 1; IRin <= 1;
+					#25 MDRin <= 0; IRin <= 0;
 				end
-			
+
 				T3: begin
 					#10 R2out <= 1; Yin <= 1;
-					#15 R2out <= 0; Yin <= 0;
+					#25 R2out <= 0; Yin <= 0;
 				end
-	
+
 				T4: begin
 					R4out <= 1; AND <= 1; Zin <= 1;
 					#15 R4out <= 0; AND <= 0; Zin <= 0;
