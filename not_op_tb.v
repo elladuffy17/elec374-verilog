a3eb99a38f8bdadcdcd813aaea9ee20f761ef16c
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-13 16:02:08 -0500

    updated not tb

--- not_op_tb.v
+++ not_op_tb.v
@@ -7,18 +7,20 @@ module not_op_tb;
 	reg PCout, ZHighout, Zlowout, MDRout, R2out, R4out; 
 	reg MARin, Zin, PCin, MDRin, IRin, Yin;
 	reg IncPC, Read;
-  reg [4:0] NOT;
-	reg R5in, R2in, R4in; //'AND' changes based on operation we want to test
+	reg [4:0] NOT;
+	reg R5in, R2in, R4in; 
 	reg HIin, LOin, ZHighIn, Cin, ZLowIn;
 	reg Clock;
 	reg [31:0] Mdatain;
+	reg Clear;
+	reg BAout;
 
 	// add any other signals to see in your simulation
 
 	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
 	reg [3:0] Present_state = Default;
 
-	cpu_phase1 DUT(PCout, ZHighout, Zlowout, MDRout, R2out, R4out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, NOT, R5in, R2in, R4in, Clock, Mdatain);
+	cpu_phase1 DUT(PCout, ZHighout, Zlowout, MDRout, R2out, R4out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, NOT, R5in, R2in, R4in, Clock, Clear, Mdatain, BAout);
 	 //the input (in.port) and output (out.port) connects the CPU to the outside world
    //the input (in.port) and output (out.port) connects the CPU to the outside world
 
@@ -65,82 +67,71 @@ module not_op_tb;
 
 	always @ (Present_state) //do the required job in each state
 		begin
+		PCout = 0; Zlowout = 0; ZHighout = 0;  MDRout= 0;   //initialize the signals
+					R2out = 0;   R4out = 0;   MARin = 0;   Zin = 0;  
+					PCin =0;   MDRin = 0;   IRin  = 0;   Yin = 0;  
+					IncPC = 0;   Read = 0;   NOT = 0;
+					R5in = 0; R2in = 0; R4in = 0; Mdatain = 32'h00000000; Clear = 0;
+					
 			case(Present_state) //assert the required signals in each clock cycle 
-
 				Default : begin
 					PCout <= 0;   Zlowout <= 0; ZHighout <= 0;  MDRout<= 0;   //initialize the signals
 					R2out <= 0;   R4out <= 0;   MARin <= 0;   ZLowIn <= 0;  
 					PCin <=0;   MDRin <= 0;   IRin  <= 0;   Yin <= 0;  
 					IncPC <= 0;   Read <= 0;   NOT <= 0;
-					R5in <= 0; R2in <= 0; R4in <= 0; Mdatain <= 32'h00000000;
+					R5in <= 0; R2in <= 0; R4in <= 0; Mdatain <= 32'h00000000; Clear = 1;
 				end
-
+	
  				Reg_load1a: begin
-					Mdatain <= 32'h00000022;
-					Read = 0; MDRin =0; //the first zero is there for completeness
-					#10 Read <= 1; MDRin <= 1;
-					#15 Read <= 0; MDRin <= 0;
+					Mdatain = 32'h00000022;
+					Read = 1; MDRin = 1; //the first zero is there for completeness
 				end
 
 				Reg_load1b: begin
-					#10 MDRout <= 1; R2in <= 1;
-					#15 MDRout <= 0; R2in <= 0; //initialize R2 with the value $22
+					 MDRout = 1; R2in = 1;
 				end
 
  				Reg_load2a: begin
-					Mdatain <= 32'h00000024;
-					#10 Read <= 1; MDRin <= 1;
-					#15 Read <= 0; MDRin <= 0;
+					Mdatain = 32'h00000024;
+					Read = 1; MDRin = 1;
 				end
 
 				Reg_load2b: begin
-					#10 MDRout <= 1; R4in <= 1;
-					#15 MDRout <= 0; R4in <= 0; //initialize R4 with the value $24
+					MDRout = 1; R4in = 1;
 				end
 
 				Reg_load3a: begin
-					Mdatain <= 32'h00000026;
-					#10 Read <= 1; MDRin <= 1;
-					#15 Read <= 0; MDRin <= 0;
+					Mdatain = 32'h00000026;
+					Read = 1; MDRin = 1;
 				end
 
 				Reg_load3b: begin
-					#10 MDRout <= 1; R5in <= 1;
-					#15 MDRout <= 0; R5in <= 0; //initialize R5 with the value $26
+					MDRout = 1; R5in = 1;
 				end
 
-				T0: begin //see if you need to de-assert these signals
-					Mdatain <= 32'h00000007;
-					PCin <= 1; MDRout <= 1;
-
-					#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1; //she has ZLowIn <= 1; but its commented out and no Zin in this line
-					#15 PCin <= 0; MDRout <= 0; PCout <= 0; MARin <= 0; IncPC <= 0;
+				T0: begin
+					PCout = 1; MARin = 1; IncPC = 1; Zin = 1; 
 				end
 
 				T1: begin
-					Mdatain <= 32'h4A920000;   
-					Read <= 1; MDRin <= 1;
-					#10 Read <= 0; MDRin <= 0;
+					Mdatain = 32'h4A920000;   
+					Read = 1; MDRin = 1;
 				end
 
 				T2: begin
-					#10 MDRin <= 1; IRin <= 1;
-					#25 MDRin <= 0; IRin <= 0;
+					MDRin = 1; IRin = 1;
 				end
 
 				T3: begin
-					#10 R2out <= 1; Yin <= 1;
-					#25 R2out <= 0; Yin <= 0;
+					R2out = 1; Yin = 1;
 				end
 
 				T4: begin
-					R4out <= 1; NOT <= 1; Zin <= 1;
-					#15 R4out <= 0; NOT <= 0; Zin <= 0;
+					R4out = 1; NOT = 5'b10001; Zin = 1;
 				end
 
 				T5: begin
-					Zlowout <= 1; R5in <= 1;
-					#15 Zlowout <= 0; R5in <= 0;
+					Zlowout = 1; R5in = 1;
 				end
 
 			endcase
