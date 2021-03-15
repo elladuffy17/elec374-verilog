6cda734022663d70841c34b4beaa42680df2be8a
elladuffy17 <72311928+elladuffy17@users.noreply.github.com>
2021-03-15 11:03:18 -0400

    not done cpu

--- cpu_phase1.v
+++ cpu_phase1.v
@@ -167,13 +167,16 @@ module cpu_phase1(
                 PCout, MDRout, inPortOut, Cout);
   
   //MAR
-	GPReg MAR(MAROut, clk, clr, MARin, busMuxOut);
+	MAR_unit MAR(MAROut, MARin, clk, clr, busMuxOut);
 	
 	//IR
 	GPReg IR(IROut, clk, clr, IRin, busMuxOut);
 	
 	//produce IR logic
-	
+	SelectAndEncodeBlock IRLogic(Gra, Grb, Grc, Rin, Rout, BAout, // signals anded with IR bits + help produce output
+	input wire [3:0] Ra, Rb, Rc, // Bits from IR
+	output R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, R0in, R1in, 
+		R2in, R3in, R4in, R5in,  R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in,C_sign_extended);
 	
 	//"CONFF" Logic
 	conff_logic CONFFLogic (CONout, CONin, IROut, busMuxOut);
