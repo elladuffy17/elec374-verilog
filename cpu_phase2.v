9612e84232c7bbed30877b4b1eb25fedcf791ffc
WillKennedy2022 <79466851+WillKennedy2022@users.noreply.github.com>
2021-03-19 16:53:29 -0400

    TB TEMPLATE

--- cpu_phase2.v
+++ cpu_phase2.v
@@ -7,7 +7,7 @@ the instructions */
 
 module cpu_phase2(
   //the input (in.port) and output (out.port) connects the CPU to the outside world
- 
+ // cpu_phase1 DUT(PCout, ZHighout, Zlowout, MDRout,R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, LD, R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in Clock, Clear, Mdatain, BAout);
    input PCout,
 	input ZHighOut,
 	input ZLowOut,
@@ -22,19 +22,16 @@ module cpu_phase2(
 	input Yin,
 	input IncPC,
 	input MDRread,
-   	input [4:0] operation,
+   input [4:0] operation,
 	input R5Enable,
 	input R2Enable,
 	input R4Enable,
 	input clk, clr,
 	input [31:0] mDataIn,
 	input BAout,
-	//section 2.5
-	input wire [31:0] inPortIn,
-	output wire [31:0] inPortOut
-
- 
-);
+	input Gra, Grb, Grc, // added ****
+	input MDRin, W_sig, BusMuxOut, BusMuxInMDROutput
+	);
   //for section 2.3
   wire [31:0] busMuxInR0_to_AND;
 	
@@ -43,30 +40,15 @@ module cpu_phase2(
   wire [4:0] encoderOutput;//select signals for MUX
   
   //define and initialize those 32 input wires for the 32-to-5 encoder
-  wire R0Out = 0;
-  wire R1Out = 0;
-  //wire R2Out = 0;
-  wire R3Out = 0;
-  //wire R4Out = 0;
-  wire R5Out = 0;
-  wire R6Out = 0;
-  wire R7Out = 0;
-  wire R8Out = 0;
-  wire R9Out = 0;
-  wire R10Out = 0;
-  wire R11Out = 0;
-  wire R12Out = 0;
-  wire R13Out = 0;
-  wire R14Out = 0;
-  wire R15Out = 0;
   wire hiOut = 0;
   wire loOut = 0;
   wire inPortOut = 0;
   wire Cout = 0;
-  wire IROut = 0;
-  wire MAROut = 0;
-  
+  wire R0Out, R1Out, R3Out, R5Out, R6Out, R7Out, R8Out, R9Out, R10Out,
+  R11Out, R12Out, R13Out, R14Out, R15Out, R0in, R1in, R2in, R3in, R4in, R5in, R6in, 
+  R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, C_sign_extended;
   reg clear;
+  wire Q, D;
   
   //connect the input wires to the encoder using the concatenation operator
   //assign statement is used since these statements are always active
@@ -105,16 +87,15 @@ module cpu_phase2(
   wire [31:0] busMuxInC;
   wire [31:0] busMuxInY;
   wire [31:0] busMuxOutMDR;
-  
   wire [31:0] ALUInZHi, ALUInZLo;
-	
-	//section 2.5 wires
-	wire [31:0] busMuxInInport; //output from inport
-	
+  
+  wire[31:0] IROut;
+
+  
   //enable signals for registers
   wire R1Enable, /*R2Enable,*/ R3Enable, /*R4Enable, R5Enable,*/ R6Enable, R7Enable, R8Enable, R9Enable, 
   R10Enable, R11Enable, R12Enable, R13Enable, R14Enable, R15Enable, hiEnable, loEnable, zHiEnable, 
-  zLoEnable, pcEnable, /*MDREnable,*/ inPortEnable, CEnable, InportEnable, OutportEnable; 
+  zLoEnable, pcEnable, /*MDREnable,*/ inPortEnable, CEnable; 
   
   wire [31:0] busMuxOut; //feed into registers as the input from the bus
   
@@ -144,20 +125,12 @@ module cpu_phase2(
   GPReg loReg(busMuxInLo, clk, clr, loEnable, busMuxOut);
   GPReg zHiReg(busMuxInZHi, clk, clr, Zin, ALUInZHi);
   GPReg zLoReg(busMuxInZLo, clk, clr, Zin, ALUInZLo);
-	GPReg pcReg(busMuxInPC, clk, IncPC, pcEnable, busMuxOut); //fix PCReg clear 
+  GPReg pcReg(busMuxInPC, clk, clr, pcEnable, busMuxOut);
   GPReg inPortReg(busMuxInInPort, clk, clr, inPortEnable, busMuxOut);
   GPReg cReg(busMuxInC, clk, clr, CEnable, busMuxOut);
   GPReg Y(busMuxInY, clk, clr, Yin, busMuxOut);
   mdrUnit MDRUnit(busMuxInMDR, busMuxOut, mDataIn, clk, clr, MDREnable, MDRread);
-  
-  //mux_2X1 MDRmux (busMuxOutMDR, busMuxOut, mDataIn, MDRread);
-  //GPReg MDRReg(busMuxInMDR, clk, clr, MDREnable, busMuxOutMDR);
-  
-  //section 2.5 - input and output ports
-	GPReg inputPort(busMuxInInport, clk, clr, InportEnable, inPortIn);
-	GPReg outputPort(inPortOut, clk, clr, OutportEnable, busMuxOut);
-	
-  //now, instantiate the bus
+
   bus phase1bus(busMuxOut, busMuxInR0, busMuxInR1, busMuxInR2, busMuxInR3, busMuxInR4,
                 busMuxInR5, busMuxInR6, busMuxInR7, busMuxInR8, busMuxInR9, busMuxInR10,
                 busMuxInR11, busMuxInR12, busMuxInR13, busMuxInR14, busMuxInR15, busMuxInHi,
@@ -166,23 +139,24 @@ module cpu_phase2(
                 R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, hiOut, loOut, ZHighOut, ZLowOut, 
                 PCout, MDRout, inPortOut, Cout);
   
-  //MAR
-	MAR_unit MAR(MAROut, MARin, clk, clr, busMuxOut);
-	
-	//IR
-	GPReg IR(IROut, clk, clr, IRin, busMuxOut);
-	
-	//produce IR logic
-	SelectAndEncodeBlock IRLogic(Gra, Grb, Grc, Rin, Rout, BAout, IROut[26:23], IROut[22:19], IROut[18:15], 
-				     //confused by this
-				     R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, R0in, R1in, 
-		R2in, R3in, R4in, R5in,  R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in,C_sign_extended);
-	
-	//"CONFF" Logic
-	conff_logic CONFFLogic (CONout, CONin, IROut, busMuxOut);
+  GPReg IR(IROut, clk, clr, IRin, busMuxOut);
   
-  //alu
   alu aluPhase1(.regZH(ALUInZHi), .regZL(ALUInZLo), .clock(clk), .clear(clr), .regA(busMuxInY), .regB(busMuxOut), .opcode(operation));
+
+  SelectAndEncodeBlock SAEB(.Gra(Gra),.Grb(Grb),.Grc(Grc), .Rin(Rin), .Rout(Rout), .BAout(BAout), .Ra(IROut[26:23]), .Rb(IROut[22:19]), .Rc(IROut[18:15]), .R0out(R0out), .R1out(R1out), .R2out(R2out), .R3out(R3out), .R4out(R4out), .R5out(R5out), .R6out(R6out), .R7out(R7out), .R8out(R8out), .R9out(R9out), .R10out(R10out), .R11out(R11out), .R12out(R12out), .R13out(R13out), .R14out(R14out), .R15out(R15out), .R0in(R0in), .R1in(R1in), .R2in(R2in), .R3in(R3in), .R4in(R4in), .R5in(R5in), .R6in(R6in), .R7in(R7in), .R8in(R8in), .R9in(R9in), .R10in(R10in), .R11in(R11in), .R12in(R12in), .R13in(R13in), .R14in(R14in), .R15in(R15in), .C_sign_extended(C_sign_extended));
   
-  endmodule
+  MemSubSystem MSS(.Clock(clk), .Clear(clr), .MARin(MARin), .MDRin(MDRin), .W_sig(W_sig), .MDRread(MDRread), .BusMuxOut(BusMuxOut), .BusMuxInMDROutput(BusMuxInMDROutput));
+
+  conff_Logic CONFFLogic (CONout, CONin, IROut, busMuxOut);
+  
+  logic_block CONFFBlock(CONout, CONin, branchFlag);
+  
+  MAR_unit MAR_inst(.Q(MAR_Q),.enable(MAR_enable), .clk(clk), .clr(clr), .D(BusMuxOut));
+  
+  RAM RAM_inst(.address(MAR_Q),.clock(clk),.data(mDataIn),.wren(W_sig),.q(Q));
+  
+  pc_reg pc1(Q, D);
+  pc_reg #(10) pc2(Q, D); // Initializing PCReg
+  
+ endmodule
   
--- ld_op_tb.v
+++ ld_op_tb.v
@@ -4,27 +4,28 @@
 
 module ld_op_tb;
 
-	reg PCout, ZHighout, Zlowout, MDRout, R2out, R4out; 
+	reg PCout, ZHighOut, ZLowOut, MDRout;
 	reg MARin, Zin, PCin, MDRin, IRin, Yin;
 	reg IncPC, Read;
 	reg [4:0]  LD;
-	reg R5in, R2in, R4in; //'AND' changes based on operation we want to test
 	reg HIin, LOin, ZHighIn, Cin, ZLowIn;
 	reg Clock;
 	reg [31:0] Mdatain;
 	reg Clear;
 	reg BAout;
-
-	// add any other signals to see in your simulation
-
-	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
+	reg Gra, Grb, Grc, Rin;
+	reg Cout;
+	reg MDREnable, MDRread, W_sig;
+	wire BusMuxOut, BusMuxInMDROutput;
+	wire R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out, R8Out, R9Out, R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, C_sign_extended;
+	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101, T7 = 4'b1110;
 	reg [3:0] Present_state = Default;
-
-	cpu_phase1 DUT(PCout, ZHighout, Zlowout, MDRout, R2out, R4out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, LD, R5in, R2in, R4in, Clock, Clear, Mdatain, BAout);
-	 //the input (in.port) and output (out.port) connects the CPU to the outside world
-   //the input (in.port) and output (out.port) connects the CPU to the outside world
-
-
+	
+	cpu_phase1 DUT(.PCout(PCout), .ZHighOut(ZHighOut), .ZLowOut(ZLowOut), .MDRout(MDRout), .R2Out(R2Out), .R4Out(R4Out), .MARin(MARin), 
+	.Zin(Zin), .PCin(PCin), .MDREnable(MDREnable), .PCin(IRin), .Yin(Yin), .IncPC(IncPC), .MDRread(MDRread), .operation(LD), .R5Enable(R5Enable), 
+	.R2Enable(R2Enable), .R4Enable(R4Enable), .clk(Clock), .clr(Clear), .mDataIn(mDataIn), .BAout(BAout), .Gra(Gra), .Grb(Grb), .Grc(Grc), 
+	.C_sign_extended(C_sign_extended), .MDRin(MDRin), .W_sig(W_sig), .BusMuxOut(BusMuxOut), .BusMuxInMDROutput(BusMuxInMDROutput));
+	
 	initial
 		begin
 			Clock = 0;
@@ -34,121 +35,57 @@ module ld_op_tb;
 	always @(posedge Clock) // finite state machine; if clock rising-edge
 		begin
 			case(Present_state)
-
-				Default			:	#40 Present_state = Reg_load1a;
-
-				Reg_load1a		:	#40 Present_state = Reg_load1b;
-
-				Reg_load1b		:	#40 Present_state = Reg_load2a;
-
-				Reg_load2a		:	#40 Present_state = Reg_load2b;
-
-				Reg_load2b		:	#40 Present_state = Reg_load3a;
-
-				Reg_load3a		:	#40 Present_state = Reg_load3b;
-
-				Reg_load3b		:	#40 Present_state = T0;
-
+				Default			:	#40 Present_state = T0;
 				T0					:	#40 Present_state = T1;
-
 				T1					:	#40 Present_state = T2;
-
 				T2					:	#40 Present_state = T3;
-
 				T3					:	#40 Present_state = T4;
-
 				T4					:	#40 Present_state = T5;
-				
 				T5					: 	#40 Present_state = T6;
-				
 				T6					: 	#40 Present_state = T7;
-
-
-
 			endcase
-
 		end
 
 	always @ (Present_state) //do the required job in each state
 		begin
-		PCout = 0; Zlowout = 0; ZHighout = 0;  MDRout= 0;   //initialize the signals
-					R2out = 0;   R4out = 0;   MARin = 0;   Zin = 0;  
-					PCin =0;   MDRin = 0;   IRin  = 0;   Yin = 0;  
-					IncPC = 0;   Read = 0;   LD = 0;
-					R5in = 0; R2in = 0; R4in = 0; Mdatain = 32'h00000000; Clear = 0;
+			PCout <= 0; ZLowOut <= 0; ZHighOut <= 0;  MDRout <= 0;
+			MARin <= 0;   Zin <= 0;  PCin <= 0;   MDRin <= 0; IRin <= 0;  
+			Yin <= 0;  IncPC <= 0;   Read <= 0; LD <= 0;  Clear <= 0;
+			Gra <= 0; Grb <= 0; MDRin <= 0;
 					
 			case(Present_state) //assert the required signals in each clock cycle 
+			
 				Default : begin
-					PCout <= 0;   Zlowout <= 0; ZHighout <= 0;  MDRout<= 0;   //initialize the signals
-					R2out <= 0;   R4out <= 0;   MARin <= 0;   ZLowIn <= 0;  
-					PCin <=0;   MDRin <= 0;   IRin  <= 0;   Yin <= 0;  
-					IncPC <= 0;   Read <= 0;   LD <= 0;
-					R5in <= 0; R2in <= 0; R4in <= 0; Mdatain <= 32'h00000000; Clear = 1;
+					PCout <= 0;   ZLowOut <= 0; ZHighOut <= 0;  MDRout<= 0;   //initialize the signals
+				   MARin <= 0;   ZLowIn <= 0; PCin <=0;   MDRin <= 0;   
+					IRin  <= 0;   Yin <= 0; IncPC <= 0;   Read <= 0; LD <= 0;
+					Clear = 1;
 				end
-	
- 				Reg_load1a: begin
-					Mdatain = 32'h00000022;
-					Read = 1; MDRin = 1; //the first zero is there for completeness
-				end
-
-				Reg_load1b: begin
-					 MDRout = 1; R2in = 1;
-				end
-
- 				Reg_load2a: begin
-					Mdatain = 32'h00000024;
-					Read = 1; MDRin = 1;
-				end
-
-				Reg_load2b: begin
-					MDRout = 1; R4in = 1;
-				end
-
-				Reg_load3a: begin
-					Mdatain = 32'h00000026;
-					Read = 1; MDRin = 1;
-				end
-
-				Reg_load3b: begin
-					MDRout = 1; R5in = 1;
-				end
-
 				T0: begin
-
-					PCout = 1; MARin = 1; IncPC = 1; Zin = 1; 
+					PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1; 
 				end
-
 				T1: begin
-					Mdatain = 32'h4A920000;   
-					Read = 1; MDRin = 1;
+					ZLowOut <= 1; PCin <= 1; Read = 1; MDRin <= 1;
 				end
-
 				T2: begin
-					MDRin = 1; IRin = 1;
+					MDRout <= 1; IRin <= 1;
 				end
-
 				T3: begin
-					R2out = 1; Yin = 1;
+					Grb <= 1; BAout <= 1; Yin <= 1;
 				end
-
 				T4: begin
-					R4out = 1; AND = 5'b01001; Zin = 1;
+					Cout <= 1; LD <= 5'b00011; Zin <= 1;
 				end
-
 				T5: begin
-					Zlowout = 1; R5in = 1;
+					ZLowOut <= 1; MARin <= 1;
 				end
-				
 				T6: begin
-
+					Read <= 1; MDRin <= 1;  
 				end
-				
 				T7: begin
-
+					MDRout <= 1; Gra <= 1; Rin <= 1;
 				end
-
 			endcase
-
 		end
-
 endmodule
+
