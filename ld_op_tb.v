03516aa2c0818869544a77180cf4e77e2055476e
WillKennedy2022 <79466851+WillKennedy2022@users.noreply.github.com>
2021-03-20 15:14:26 -0400

    TB UPDATED

--- ld_op_tb.v
+++ ld_op_tb.v
@@ -1,30 +1,28 @@
 //and_op_tb.v, testbench for the AND operation module 
-
 `timescale 1ns/10ps
-
 module ld_op_tb;
-
 	reg PCout, ZHighOut, ZLowOut, MDRout;
 	reg MARin, Zin, PCin, MDRin, IRin, Yin;
 	reg IncPC, Read;
-	reg [4:0]  LD;
+	reg [4:0] LD;
 	reg HIin, LOin, ZHighIn, Cin, ZLowIn;
 	reg Clock;
 	reg [31:0] Mdatain;
 	reg Clear;
 	reg BAout;
-	reg Gra, Grb, Grc, Rin;
+	reg Gra, Grb, Grc;
 	reg Cout;
 	reg MDREnable, MDRread, W_sig;
+	reg Rin, Rout, hiEnable, loEnable, pcEnable, inPortEnable, CEnable, CONout, CONin, MAR_enable;
 	wire BusMuxOut, BusMuxInMDROutput;
 	wire R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out, R8Out, R9Out, R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, C_sign_extended;
 	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101, T7 = 4'b1110;
 	reg [3:0] Present_state = Default;
 	
-	cpu_phase1 DUT(.PCout(PCout), .ZHighOut(ZHighOut), .ZLowOut(ZLowOut), .MDRout(MDRout), .R2Out(R2Out), .R4Out(R4Out), .MARin(MARin), 
-	.Zin(Zin), .PCin(PCin), .MDREnable(MDREnable), .PCin(IRin), .Yin(Yin), .IncPC(IncPC), .MDRread(MDRread), .operation(LD), .R5Enable(R5Enable), 
-	.R2Enable(R2Enable), .R4Enable(R4Enable), .clk(Clock), .clr(Clear), .mDataIn(mDataIn), .BAout(BAout), .Gra(Gra), .Grb(Grb), .Grc(Grc), 
-	.C_sign_extended(C_sign_extended), .MDRin(MDRin), .W_sig(W_sig), .BusMuxOut(BusMuxOut), .BusMuxInMDROutput(BusMuxInMDROutput));
+	cpu_phase2 DUT(.PCout(PCout), .ZHighOut(ZHighOut), .ZLowOut(ZLowOut), .MDRout(MDRout), .R2Out(R2Out), .R4Out(R4Out), .MARin(MARin), 
+	.Zin(Zin), .PCin(PCin), .MDREnable(MDREnable), .IRin(IRin), .Yin(Yin), .IncPC(IncPC), .MDRread(MDRread), .operation(LD), .clk(Clock),
+	.clr(Clear), .BAout(BAout), .Gra(Gra), .Grb(Grb), .Grc(Grc), .MDRin(MDRin), .W_sig(W_sig),
+	.BusMuxOut(BusMuxOut), .BusMuxInMDROutput(BusMuxInMDROutput), .Rin(Rin), .Rout(Rout), .hiEnable(hiEnable), .loEnable(loEnable), .pcEnable(pcEnable), .inPortEnable(inPortEnable), .CEnable(CEnable), .CONout(CONout), .CONin(CONin), .MAR_enable(MAR_enable)); 
 	
 	initial
 		begin
@@ -74,7 +72,7 @@ module ld_op_tb;
 					Grb <= 1; BAout <= 1; Yin <= 1;
 				end
 				T4: begin
-					Cout <= 1; LD <= 5'b00011; Zin <= 1;
+					Cout <= 1; LD <= 5'b0000; Zin <= 1;
 				end
 				T5: begin
 					ZLowOut <= 1; MARin <= 1;
