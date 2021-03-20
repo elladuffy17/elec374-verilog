684653961dc3e4e6a4b24813646c591b6bb5ba99
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-03-20 16:26:17 -0400

    Update branch_op_tb.v

--- branch_op_tb.v
+++ branch_op_tb.v
@@ -15,6 +15,7 @@ module branch_op_tb;
 	reg Cout;
 	reg MDREnable, MDRread, W_sig;
 	reg Rin, Rout, hiEnable, loEnable, pcEnable, inPortEnable, CEnable, CONout, CONin, MAR_enable;
+	wire CON;
 	wire BusMuxOut, BusMuxInMDROutput;
 	wire R0Out, R1Out, R2Out, R3Out, R4Out, R5Out, R6Out, R7Out, R8Out, R9Out, R10Out, R11Out, R12Out, R13Out, R14Out, R15Out, R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, C_sign_extended;
 	parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011, Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111, T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100, T6 = 4'b1101, T7 = 4'b1110;
@@ -23,7 +24,7 @@ module branch_op_tb;
 	cpu_phase2 DUT(.PCout(PCout), .ZHighOut(ZHighOut), .ZLowOut(ZLowOut), .MDRout(MDRout), .R2Out(R2Out), .R4Out(R4Out), .MARin(MARin), 
 	.Zin(Zin), .PCin(PCin), .MDREnable(MDREnable), .IRin(IRin), .Yin(Yin), .IncPC(IncPC), .MDRread(MDRread), .operation(LD), .clk(Clock),
 	.clr(Clear), .BAout(BAout), .Gra(Gra), .Grb(Grb), .Grc(Grc), .MDRin(MDRin), .W_sig(W_sig),
-	.BusMuxOut(BusMuxOut), .BusMuxInMDROutput(BusMuxInMDROutput), .Rin(Rin), .Rout(Rout), .hiEnable(hiEnable), .loEnable(loEnable), .pcEnable(pcEnable), .inPortEnable(inPortEnable), .CEnable(CEnable), .CONout(CONout), .CONin(CONin), .MAR_enable(MAR_enable)); 
+	.CON(CON), .BusMuxOut(BusMuxOut), .BusMuxInMDROutput(BusMuxInMDROutput), .Rin(Rin), .Rout(Rout), .hiEnable(hiEnable), .loEnable(loEnable), .pcEnable(pcEnable), .inPortEnable(inPortEnable), .CEnable(CEnable), .CONout(CONout), .CONin(CONin), .MAR_enable(MAR_enable)); 
 	
 	initial
 		begin
@@ -55,8 +56,9 @@ module branch_op_tb;
 			
 				Default : begin
 					PCout <= 0;   ZLowOut <= 0; ZHighOut <= 0;  MDRout<= 0;   //initialize the signals
-				  	MARin <= 0;   ZLowIn <= 0; PCin <=0;   MDRin <= 0;   
-					IRin  <= 0;   Yin <= 0; IncPC <= 0;   Read <= 0; ADD <= 0;
+				  	MARin <= 0;   ZLowIn <= 0; PCin <=0;   MDRin <= 0; Zin <= 0;
+					IRin  <= 0;   Yin <= 0; IncPC <= 0; Yin <= 0; Read <= 0; ADD <= 0;
+					Gra <= 0; Rin <= 0; Rout <= 0; CONin <= 0;
 					Clear = 1;
 				end
 				T0: begin
@@ -69,16 +71,17 @@ module branch_op_tb;
 					MDRout <= 1; IRin <= 1;
 				end
 				T3: begin
-					Gra <= 1; Rout <= 1; CONin <= 1;
+					Gra <= 1; Rout <= 1; CONin <= 1; BAout <= 1;
 				end
 				T4: begin
-					PCout <= 1; Yin <= 1;
+					PCout <= 1; Zin <= 1;
 				end
 				T5: begin
-					Cout <= 1; ADD <= 5'b00011; Zin <= 1;
+					Cout <= 1; ADD <= 5'b00011; Yin <= 1;
 				end
 				T6: begin
-					ZLowOut <= 1; PC <= CON; // I HAVE NO IDEA
+					if (CON == 1) begin
+					ZLowOut <= 1; PCin <= 0; 
 				end
 			endcase
 		end
