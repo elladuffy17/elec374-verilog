1601e92cea2d36e1ac626f5e1bfd2abc16e422b3
hannahyeatman <65246254+hannahyeatman@users.noreply.github.com>
2021-04-09 14:35:26 -0400

    Create phase3_tb.v

---
+++ phase3_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns/10ps
+
+module phase3_tb;
+	reg clk, rst, stop;
+	wire[31:0] InPort_input, OutPort_output, bus_contents;
+	wire [4:0] operation;
+
+cpu_phase3 DUT(
+	.clock(clk),
+  .Reset(Reset),
+  .Stop(Stop),
+  .InPort_input(InPort_input), //idk about these
+  .OutPort_output(OutPort_output), //idk about these
+	.bus_contents(bus_contents),
+	.operation(operation)
+);
+
+initial
+	begin
+		clk = 0;
+		Reset = 0;
+end
+
+always
+		#10 clk <= ~clk;
+
+endmodule
